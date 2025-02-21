72b329e100e2daee5732b8b96b12b63e4f30aaa8
unknown <343025052@TDSB.ON.CA>
2025-02-21 11:36:16 -0500

    Boolean Test Update

---
+++ src/BooleanTest.java
@@ -0,0 +1,25 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class BooleanTest {
+    public static void main(String[] args) {
+        // Ask the user or two boolean varibles
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your A value");
+        boolean A = scanner.nextBoolean();
+        System.out.println("Please enter your B value");
+        boolean B = scanner.nextBoolean();
+        System.out.println("Please enter your C value");
+        boolean C = scanner.nextBoolean();
+        System.out.println("Please enter your D value");
+        boolean D = scanner.nextBoolean();
+        
+        // NAND output
+        boolean output = (B && !D) || (!B && !C && D) || (!A && !B && !C) || (A && C && !D);
+        System.out.println("Your value is " + output);
+        scanner.close();
+
+    }
+}
+
---
+++ src/HeronsFormula.java
@@ -0,0 +1,21 @@
+package Assignments;
+import java.util.Scanner;
+
+public class HeronsFormula {
+    public static void main(String[] args) {
+        // Ask the user for three sides of the triangle
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter your a value");
+        double a = keyboard.nextDouble();
+        System.out.println("Please enter your b value");
+        double b = keyboard.nextDouble();        
+        System.out.println("Please enter your c value");
+        double c = keyboard.nextDouble();
+        
+        // Using HeronsFormula
+        double area = Math.sqrt(4 * a * a * b * b - Math.pow(a * a + b * b - c * c, 2)) / 4;
+        System.out.println("The area of the triangle is: " + area);
+        keyboard.close();
+
+    }
+}
