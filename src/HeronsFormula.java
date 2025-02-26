d1363aeb6d99e3fe66fd837e79284d3e9bcb59fd
unknown <343025052@TDSB.ON.CA>
2025-02-26 11:58:57 -0500

    Newtons Method Update

--- src/HeronsFormula.java
+++ src/HeronsFormula.java
@@ -7,14 +7,13 @@ public class HeronsFormula {
         System.out.println("Please enter your a value: ");
         double a = keyboard.nextDouble();
         System.out.println("Please enter your b value: ");
-        double b = keyboard.nextDouble();        
+        double b = keyboard.nextDouble();
         System.out.println("Please enter your c value: ");
         double c = keyboard.nextDouble();
-        
+
         // Using HeronsFormula
         double area = Math.sqrt(4 * a * a * b * b - Math.pow(a * a + b * b - c * c, 2)) / 4;
         System.out.println("The area of the triangle is: " + area);
         keyboard.close();
-
     }
 }
