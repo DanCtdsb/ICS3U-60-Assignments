65668c5baca5881d4db526355fa1b3785a8ff118
Chen <Daniel.Chen21@student.tdsb.on.ca>
2025-02-28 11:40:16 -0500

    test

---
+++ src/NewtonsMethod.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class NewtonsMethod {
+    public static void main(String[] args) {
+        // Ask the user for the n value
+        Scanner scanner = new Scanner(System.in);
+        char input;
+        // Do the following once, and continue as long as input is y
+        do {
+            System.out.println("Please enter your inital guess");
+            double guess = scanner.nextDouble();
+            double prev_guess = guess + 1;
+
+            System.out.println(Math.abs(prev_guess - guess) < 0.0001);
+            while (Math.abs(prev_guess - guess) > 0.000000001) {
+                prev_guess = guess;
+                double fx = 6 * Math.pow(guess, 4) - 13 * Math.pow(guess, 3) - 18 * Math.pow(guess, 2) + 7 * guess + 6;
+                double fpx = 24 * Math.pow(guess, 3) - 39 * Math.pow(guess, 2) - 36 * guess + 7;
+                guess = prev_guess - fx/fpx;
+                System.out.println(guess);
+            }
+
+            // Print out the Root
+            System.out.println("Your approximate root is " + guess);
+            System.out.println("Do you want to try again?");
+            input = scanner.next().toLowerCase().charAt(0);
+        } while (input == 'y');
+        scanner.close();
+    }
+}
