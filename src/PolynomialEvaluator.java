24eea73f88a9198363023849dee89f04b9bd86ee
DanCtdsb <daniel.chen21@student.tdsb.on.ca>
2025-03-03 17:08:38 -0500

    Update PolynomialEvaluator.java

--- src/PolynomialEvaluator.java
+++ src/PolynomialEvaluator.java
@@ -14,13 +14,39 @@ public class PolynomialEvaluator {
         StringTokenizer st = new StringTokenizer(polynomial, " ");
         System.out.println("Please enter your x value: ");
         double x = keyboard.nextDouble();
+        double result = 0;
         
         while (st.hasMoreTokens()) {
             String current_token = st.nextToken();
+            String coefficient = "";
+            String exponent = "";
+            boolean seen = false;
             for (int i = 0; i < current_token.length(); i++) {
                 char character = current_token.charAt(i);
+                if (character == '+' || character == '-') {
+                    continue;
+                }
+                if (i + 1 == current_token.length()) {
+                    exponent += character;
+                    if (coefficient == "") {
+                        result += character - '0';
+                    } else if (!seen) {
+                        result += Integer.parseInt(coefficient) * x;
+                    } else {
+                        result += Integer.parseInt(coefficient) * Math.pow(x, Integer.parseInt(exponent));
+                    }
+                } else if (Character.isDigit(character)) {
+                    if (seen) {
+                        exponent += character;
+                    } else {
+                        coefficient += character;
+                    }
+                } else if (character == '^') {
+                    seen = true;
+                }
             }
         }
+        System.out.println("Result" + result);
         keyboard.close();
     }
 }
