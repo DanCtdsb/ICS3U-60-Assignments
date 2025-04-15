0045d62815e01e8e35e4f2815d5e7ffa80bf4734
Chen <Daniel.Chen21@student.tdsb.on.ca>
2025-04-15 10:53:21 -0400

    Country Enum

---
+++ src/Enum/Country.java
@@ -0,0 +1,35 @@
+package Enum;
+
+public enum Country {
+    
+    CANADA(9984670, "CAN"),
+    UNITED_STATES_OF_AMERCA(9372610, "USA"),
+    BAHAMAS(13878, "BHS"),
+    KENYA(582646 , "KEN"),
+    HAITI(27750, "HTI"),
+    MEXICO(1972550, "MEX"),
+    SLOVENIA(20271, "SVN"),
+    MALAYSIA(330803, "MYS"),
+    PARAGUAY(406752, "PRY"),
+    NEW_ZEALAND(268021, "NZL");
+
+    private double landMass;
+    private String countryCode;
+
+    Country(double landMass, String countryCode) {
+        this.landMass = landMass;
+        this.countryCode = countryCode;
+    }
+    
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public double getLandMass() {
+        return landMass;
+    }
+
+    public double compareLandMass(Country country) {
+        return this.landMass / country.getLandMass();
+    }
+}
--- src/Enum/Main.java
+++ src/Enum/Main.java
@@ -1,5 +1,12 @@
 package Enum;
 
 public class Main {
-    
+
+    public static void main(String[] args) {
+        for (Country country : Country.values()) {
+            System.out.println("");
+            System.out.println("Land mass of " + country + " : " + country.getLandMass());
+            System.out.println("The ratio of land mass between " + country + " and HAITI is " + country.compareLandMass(Country.HAITI));
+        }
+    }
 }
