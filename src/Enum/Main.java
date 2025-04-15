019ae5025e804ea81cd07942a59d149b620c3f19
unknown <343025052@TDSB.ON.CA>
2025-04-15 11:37:15 -0400

    d

--- src/Enum/Main.java
+++ src/Enum/Main.java
@@ -6,8 +6,8 @@ public class Main {
         double population = 38940000;
         for (Country country : Country.values()) {
             System.out.println("");
-            System.out.println("Land mass of " + country + " : " + country.getLandMass());
             System.out.println("Country code of " + country + " : " + country.getCountryCode());
+            System.out.println("Land mass of " + country + " : " + country.getLandMass());
             System.out.println("The ratio of land mass between " + country + " and HAITI is " + country.compareLandMass(Country.HAITI));
             System.out.println("Population Density of " + country + " : " + country.PopulationDensity(population) + "Ppl/Km^2");
         }
