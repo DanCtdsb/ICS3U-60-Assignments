0fa51e6a3fec9828cf2cfcc130479aac2b198372
Chen <Daniel.Chen21@student.tdsb.on.ca>
2025-04-17 11:29:30 -0400

    Update

--- src/Enum/Main.java
+++ src/Enum/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         double population = 38940000;
         for (Country country : Country.values()) {
-            System.out.println("");
+            System.out.println("--------------------------");
             System.out.println("Country code of " + country + " : " + country.getCountryCode());
             System.out.println("Land mass of " + country + " : " + country.getLandMass());
             System.out.println("The ratio of land mass between " + country + " and HAITI is " + country.compareLandMass(Country.HAITI));
