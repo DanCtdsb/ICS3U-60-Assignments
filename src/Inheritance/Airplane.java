e702578ba4233e08fa69454c0d56626423b64e72
DanCtdsb <daniel.chen21@student.tdsb.on.ca>
2025-04-30 09:30:58 -0400

    Update Airplane.java

--- src/Inheritance/Airplane.java
+++ src/Inheritance/Airplane.java
@@ -13,7 +13,7 @@ public class Airplane extends Aircraft {
     * @param avgSpeed the average speed of the system, in m/s
     * @param altitute the current altitute the aircraft is at, in m
     * @param wingspan the wingspan(length from one wing to the other) of the airplane, in m
-    * @param thrust the thrust force, in lb
+    * @param thrust the thrust force of the engines, in lb
     * @param chordLen the chord length, in m
     */
     public Airplane(String name, double fuel, double avgSpeed, double altitute, double wingspan, double thrust,
@@ -51,55 +51,100 @@ public class Airplane extends Aircraft {
     /**  
     * Finds the area of the wings
     *
-    * @return 
+    * @return the wing area
     */
     public double wingArea() {
         return wingspan * chordLen;
     }
-
+    /**  
+    * Finds the aspect ratio of the airplane
+    *
+    * @return the aspect ratio
+    */
     public double aspectRatio() {
         return wingspan * wingspan / wingArea();
     }
-
+    /**  
+    * Finds the lift coefficient based on the lift force based on Cl = L / (A * .5 * r * V^2)
+    *
+    * @param liftForce the lift force, in N
+    * @return the lift coefficient
+    */
     public double liftCoefficient(double liftForce) {
         return liftForce / (wingArea() * 0.5 * 1.225 * avgSpeed * avgSpeed);
     }
-
+    /**  
+    * Finds the drag coefficient based on Cdi = (Cl^2) / (π * AR * e), where e is 1, and r is 1.225
+    *
+    * @param liftForce the lift force, in N
+    * @return the drag coefficient
+    */
     public double dragCoefficient(double liftForce) {
         return Math.pow(liftCoefficient(liftForce), 2) / (Math.PI * aspectRatio() * 1);
     }
-    // Cdi = (Cl^2) / (π * AR * e), Cl = L / (A * .5 * r * V^2)
-    // e is 1 for ideal case, ask for l, r is about 1.225
-
+    /**  
+    * Finds the drag Force based on D = (1/2) * Cd * ρ * A * V^2
+    *
+    * @param liftForce the lift force, in N
+    * @return the drag Force
+    */
     public double dragForce(double liftForce) {
         return 0.5 * dragCoefficient(liftForce) * 1.225 * wingArea() * Math.pow(avgSpeed, 2);
     }
-    // D = (1/2) * Cd * ρ * A * V^2, p is density, 1.225, Cd is the dragcoeff
-
+     /**  
+    * Finds the excess Thrust based on the liftForce
+    *
+    * @param liftForce the lift force, in N
+    * @return the excess thrust
+    */
     public double excessThrust(double liftForce) {
         return thrust - dragForce(liftForce) / 4.448;
     }
-
+    /**  
+    * Gets the wingspan of the airplane
+    *
+    * @return the wingspan
+    */
     protected double getWingSpan() {
         return wingspan;
     } 
-
+    /**  
+    * Changes the wingspan of the airplane
+    *
+    * @param wingspan of the airplane, in m
+    */
     protected void setWingSpan(double wingspan) {
         this.wingspan = wingspan;
     } 
-
+    /**  
+    * Gets the thrust of the airplane's engines
+    *
+    * @return the thrust
+    */
     protected double getThrust() {
         return thrust;
     } 
-
+    /**  
+    * Changes the thrust of the airplane
+    *
+    * @param thrust of the airplane, in N
+    */
     protected void setThrust(double thrust) {
         this.thrust = thrust;
     } 
-
+    /**  
+    * Gets the chord length of the airplane
+    *
+    * @return the chord length, in m
+    */
     protected double getChordLen() {
         return chordLen;
     } 
-
+    /**  
+    * Changes the chord length of the airplane
+    *
+    * @param chord length of the airplane, in m
+    */
     protected void setAltitute(double chordLen) {
         this.chordLen = chordLen;
     } 
